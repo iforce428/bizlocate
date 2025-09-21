fdd82f248f680b762e325227588d0c79798c2a50
iforce428 <iforce428@gmail.com>
2025-09-21 15:35:40 +0800

    Use Lambda function URL

--- api/bedrock-chat.js
+++ api/bedrock-chat.js
@@ -12,7 +12,15 @@ export const handler = async (event) => {
   };
 
   if (event.httpMethod === 'OPTIONS' || event.requestContext?.http?.method === 'OPTIONS') {
-    return { statusCode: 200, headers, body: '' };
+    return { 
+      statusCode: 200, 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'POST,OPTIONS'
+      }, 
+      body: '' 
+    };
   }
 
   try {
